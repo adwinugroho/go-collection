1e6e4c175276a2a6a0116b27b411011477f025eb
adwin nugroho <adwinnugroho16@gmail.com>
2020-12-15 14:58:22 +0700

    add csv file

---
+++ csv_file/services.go
@@ -0,0 +1,61 @@
+package csv_file
+
+import (
+	"encoding/csv"
+	"log"
+	"os"
+	"path"
+	"strconv"
+)
+
+type interfaceSvcCsvFile struct{}
+
+type User struct {
+	ID       string `json:"id"`
+	Username string `json:"username"`
+	Email    string `json:"email"`
+}
+
+var users []User
+
+func GetSvcCsvFile() *interfaceSvcCsvFile {
+	return &interfaceSvcCsvFile{}
+}
+
+func (service *interfaceSvcCsvFile) CreateNewCsvFile() error {
+
+	users = append(users, User{ID: "1", Username: "Pangeran", Email: "bangpange@gmail.com"})
+	users = append(users, User{ID: "2", Username: "Sihaan", Email: "bangsihaan@gmail.com"})
+
+	var dataCSV [][]string
+	var empData []string
+	var initialHeader []string
+
+	initialHeader = append(initialHeader, "No", "ID", "Username", "E-Mail")
+	dataCSV = append(dataCSV, initialHeader)
+	var count int
+	for _, item := range users {
+		count++
+		empData = []string{strconv.Itoa(count), item.ID, item.Username, item.Email}
+		dataCSV = append(dataCSV, empData)
+	}
+
+	dir := "upload"
+	os.Mkdir(dir, 0666)
+	fileName := path.Join(dir, "file.csv")
+	csvFile, err := os.Create(fileName)
+	if err != nil {
+		log.Printf("failed creating file: %s", err)
+		return err
+	}
+	defer csvFile.Close()
+
+	csvwriter := csv.NewWriter(csvFile)
+	defer csvwriter.Flush()
+
+	for _, empRow := range dataCSV {
+		_ = csvwriter.Write(empRow)
+	}
+
+	return nil
+}
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 
+	csvFile "github.com/adwinugroho/go-collection/csv_file"
 	"github.com/adwinugroho/go-collection/remove_array_element"
 )
 
@@ -11,5 +12,9 @@ func main() {
 	fmt.Println("===========================")
 	getSvcRemoveElement := remove_array_element.GetSvcRemoveElementArr()
 	getSvcRemoveElement.RemoveArrayElement()
+	err := csvFile.GetSvcCsvFile().CreateNewCsvFile()
+	if err != nil {
+		panic(err)
+	}
 
 }
