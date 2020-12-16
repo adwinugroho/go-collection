3fce867f9bfff98901450e6e9b416cb6d510ef0e
adwin nugroho <adwinnugroho16@gmail.com>
2020-12-16 19:15:51 +0700

    add count duplicate element in array

---
+++ count_duplicate_element/services.go
@@ -0,0 +1,18 @@
+package count_duplicate_element
+
+// ex: list [hello, how, are, today, you, fine]
+// result gonna be like hello: 1, how: 1, are: 1 today: 1 and you: 2
+func CountDuplicateElement(list []string) map[string]int {
+	DuplicateElement := make(map[string]int)
+	// looping list
+	for _, item := range list {
+		// check if element found in map
+		_, exist := DuplicateElement[item]
+		if exist {
+			DuplicateElement[item] += 1 // iteration
+		} else {
+			DuplicateElement[item] = 1
+		}
+	}
+	return DuplicateElement
+}
